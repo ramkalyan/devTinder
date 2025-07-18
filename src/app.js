b3b56215a3b88b776ec776f8e0fa15353979edf7
ramkalyan-reddy <ramkalyan_reddy@apple.com>
2025-07-18 18:36:24 +0530

    Exploring get, POST

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    res.send("Im learning node. Bless me");
+app.use("/hello", (req, res) => {
+    res.send("Im hello node. Hello");
 })
-app.use("/", (req, res) => {
-    res.send("Im learning node. Bless me");
-})
-app.use("hello", (req, res) => {
-    res.send("Im learning node. Bless me");
-})
+app.get("/user", (req,res) => {
+    res.send({firstName: "Ram", lastName: "Kalyan"});
+});
+app.post("/user", (req,res) => {
+    res.send("Data saved to Database");
+});
+// app.use("/", (req, res) => {
+//     res.send("Im learning node. Bless me");
+// })
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777..")
 });
