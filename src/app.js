f3a9dcc0d36e3d39d83b75eab6eeb6dc668c2839
ramkalyan-reddy <ramkalyan_reddy@apple.com>
2025-07-18 17:49:34 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const app = express();
 app.use((req, res) => {
     res.send("Im learning node. Bless me");
 })
+app.use("/", (req, res) => {
+    res.send("Im learning node. Bless me");
+})
+app.use("hello", (req, res) => {
+    res.send("Im learning node. Bless me");
+})
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777..")
 });
